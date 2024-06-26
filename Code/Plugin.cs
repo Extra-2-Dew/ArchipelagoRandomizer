ee34196a12d17abaa63c9ac218b7777065643770
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-06-26 00:21:08 -0700

    Setup project

--- Code/Plugin.cs
+++ Code/Plugin.cs
@@ -1,14 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
 
 namespace ArchipelagoRandomizer
 {
 	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInDependency("ModCore")]
 	public class Plugin : BaseUnityPlugin
 	{
+		internal static ManualLogSource Log { get; private set; }
+
 		private void Awake()
 		{
-			// Plugin startup logic
+			Log = Logger;
 			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 		}
 	}
 }
