25c09282dbd44adfe0bfef1c7732f8739af5c1d5
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-06-30 23:23:20 -0700

    Start with full maps and warps unlocked

--- Code/ItemRandomizer.cs
+++ Code/ItemRandomizer.cs
@@ -14,8 +14,6 @@ namespace ArchipelagoRandomizer
 		public bool HasInitialized { get; private set; }
 		public bool IsActive { get; private set; }
 
-		//ap /connect localhost:38281 WyrmID
-
 		public ItemRandomizer(bool newFile)
 		{
 			instance = this;
@@ -43,6 +41,92 @@ namespace ArchipelagoRandomizer
 		{
 			SaverOwner saver = ModCore.Plugin.MainSaver;
 			saver.LocalStorage.GetLocalSaver("settings").SaveInt("hideCutscenes", 1);
+			saver.SaveAll();
+			UnlockWarpGarden();
+			ObtainedTracker3();
+		}
+
+		private void UnlockWarpGarden()
+		{
+			List<string> warpLetters = new() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+			SaverOwner saver = ModCore.Plugin.MainSaver;
+			saver.WorldStorage.SaveInt("WorldWarpE", 1);
+
+			for (int i = 0; i < warpLetters.Count; i++)
+			{
+				string warp = "WorldWarp" + warpLetters[i];
+				saver.WorldStorage.SaveInt(warp, 1);
+				saver.GetSaver("/local/markers").SaveData(warp, warp);
+			}
+
+			saver.SaveAll();
+		}
+
+		private void ObtainedTracker3()
+		{
+			Dictionary<string, string[]> scenesAndRooms = new()
+			{
+				{ "FluffyFields", new string[] { "A", "B", "C" } },
+				{ "CandyCoast", new string[] { "A", "B", "C" } },
+				{ "FancyRuins", new string[] { "C", "B", "A" } },
+				{ "FancyRuins2", new string[] { "A" } },
+				{ "StarWoods", new string[] { "A", "B", "C" } },
+				{ "StarWoods2", new string[] { "A" } },
+				{ "SlipperySlope", new string[] { "A", "B" } },
+				{ "VitaminHills", new string[] { "A", "B", "C" } },
+				{ "VitaminHills2", new string[] { "A" } },
+				{ "VitaminHills3", new string[] { "A" } },
+				{ "FrozenCourt", new string[] { "A" } },
+				{ "LonelyRoad", new string[] { "A", "B", "C", "D", "E" } },
+				{ "LonelyRoad2", new string[] { "A" } },
+				{ "Deep2", new string[] { "A", "B", "C", "D" } },
+				{ "Deep3", new string[] { "B", "A" } },
+				{ "Deep4", new string[] { "A", "B" } },
+				{ "Deep5", new string[] { "A", "B", "C", "D", "E", "F", "G" } },
+				{ "Deep6", new string[] { "B", "A" } },
+				{ "Deep7", new string[] { "A", "B" } },
+				{ "Deep8", new string[] { "A", "B", "C", "D" } },
+				{ "Deep9", new string[] { "B", "A" } },
+				{ "Deep10", new string[] { "B", "A" } },
+				{ "Deep11", new string[] { "A", "B", "C", "D", "E", "F" } },
+				{ "Deep12", new string[] { "A", "B", "C" } },
+				{ "Deep13", new string[] { "A", "B", "C", "D", "E" } },
+				{ "Deep14", new string[] { "A", "B", "C", "D", "E", "F" } },
+				{ "Deep15", new string[] { "B", "A" } },
+				{ "Deep17", new string[] { "A", "B" } },
+				{ "Deep19s", new string[] { "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T" } },
+				{ "Deep20", new string[] { "A", "B" } },
+				{ "Deep22", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P" } },
+				{ "PillowFort", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" } },
+				{ "SandCastle", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" } },
+				{ "ArtExhibit", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S" } },
+				{ "TrashCave", new string[] { "J", "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T" } },
+				{ "FloodedBasement", new string[] { "M", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "O", "P", "Q", "R", "S", "T", "U" } },
+				{ "PotassiumMine", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U" } },
+				{ "BoilingGrave", new string[] { "V", "A", "AA", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z" } },
+				{ "GrandLibrary", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE" } },
+				{ "GrandLibrary2", new string[] { "BA", "BB", "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "DA", "DB", "DC", "DD", "DE", "DF", "DG", "DH", "EA" } },
+				{ "SunkenLabyrinth", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U" } },
+				{ "MachineFortress", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R" } },
+				{ "DarkHypostyle", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W" } },
+				{ "TombOfSimulacrum", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ" } },
+				{ "DreamForce", new string[] { "B", "C", "E", "I", "J", "K", "L", "M", "Y", "W", "X", "Z", "AA", "AE", "AF", "AG", "AH", "AD" } },
+				{ "DreamDynamite", new string[] { "A", "B", "C", "D", "E", "F", "I", "K", "L", "N", "R", "V", "W", "X", "Y", "Z", "AB", "AF", "AG", "AH", "AI", "AL", "AM", "AN", "AO", "AS", "AT", "AU" } },
+				{ "DreamFireChain", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R" } },
+				{ "DreamIce", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "AA", "Z", "AB", "AC", "AD", "AE" } },
+				{ "DreamAll", new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD" } },
+			};
+			SaverOwner saver = ModCore.Plugin.MainSaver;
+
+			// Mark all rooms as visited
+			foreach (KeyValuePair<string, string[]> sceneAndRoom in scenesAndRooms)
+			{
+				for (int i = 0; i < sceneAndRoom.Value.Length; i++)
+				{
+					saver.GetSaver($"/local/levels/{sceneAndRoom.Key}/player/seenrooms").SaveInt(sceneAndRoom.Value[i], 1);
+				}
+			}
+
 			saver.SaveAll();
 		}
 
