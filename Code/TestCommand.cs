89cc42b21953572d32bfa202db71001743368962
GameWyrm <daronan97@hotmail.com>
2024-08-11 22:55:29 -0700

    New crystal chest assets and test command

--- Code/TestCommand.cs
+++ Code/TestCommand.cs
@@ -9,41 +9,65 @@ namespace ArchipelagoRandomizer
         private static TestCommand instance;
         public static TestCommand Instance { get { return instance; } }
 
-        private string[] rimColors =
+        private string[] crystalEdgeRimRamps =
         {
-            "ChestDarkYellow",
-            "ChestDarkYellow",
-            "ChestGrey",
-            "ChestGrey",
-            "ChestGrey",
-            "ChestDarkYellow",
-            "ChestGrey",
-            "ChestDarkYellow"
+            "ChestCrystalRimGold",
+            "ChestCrystalRimGold",
+            "ChestCrystalRimSilver",
+            "ChestCrystalRimSilver",
+            "ChestCrystalRimSilver",
+            "ChestCrystalRimGold",
+            "ChestCrystalRimSilver",
+            "ChestCrystalRimGold"
         };
-        private string[] rimShines =
+        private string[] crystalEdgeRims =
         {
-            "ChestRimGold",
-            "ChestRimGold",
-            "ChestRimGrey",
-            "ChestRimGrey",
-            "ChestRimGrey",
-            "ChestRimGold",
-            "ChestRimGrey",
-            "ChestRimGold"
+            "ChestJewelGold",
+            "ChestJewelGold",
+            "ChestJewelSilver",
+            "ChestJewelSilver",
+            "ChestJewelSilver",
+            "ChestJewelGold",
+            "ChestJewelSilver",
+            "ChestJewelGold"
         };
-        private string[] chestColors =
+        private string[] crystalFaceRamps =
         {
-            "ChestTealGreen",
-            "ChestBrown",
-            "ChestBurgundy",
-            "ChestDarkGrey",
-            "ChestTealGreen",
-            "ChestBlue",
-            "ChestOrange",
-            "ChestOrange"
+            "ChestJewelGreen",
+            "ChestJewelBrown",
+            "ChestJewelBurgundy",
+            "ChestJewelDarkGrey",
+            "ChestJewelGreen",
+            "ChestJewelBlue",
+            "ChestJewelOrange",
+            "ChestJewelOrange"
         };
-        private Material rimMaterial;
-        private Material chestMaterial;
+        private string[] crystalFaceRims =
+        {
+            "ChestCrystalRimGreen",
+            "ChestCrystalRimBrown",
+            "ChestCrystalRimBurgundy",
+            "ChestCrystalRimDarkGrey",
+            "ChestCrystalRimGreen",
+            "ChestCrystalRimBlue",
+            "ChestCrystalRimOrange",
+            "ChestCrystalRimOrange"
+        };
+        private string[] crystalEdgeColors =
+        {
+            "ChestYellow",
+            "ChestYellow",
+            "ChestLightGrey",
+            "ChestLightGrey",
+            "ChestLightGrey",
+            "ChestYellow",
+            "ChestLightGrey",
+            "ChestYellow"
+        };
+        // Crystal edges
+        private Material edgeMaterial;
+        // Crystal faces
+        private Material faceMaterial;
 
         public TestCommand()
         {
@@ -58,19 +82,26 @@ namespace ArchipelagoRandomizer
         [DebugMenuCommand(commandName:"test")]
         private void SendTestCommand(string[] args)
         {
-            if (rimMaterial == null)
+            if (edgeMaterial == null)
             {
-                Renderer chestRenderer = GameObject.Find("Dungeon_Chest").GetComponentInChildren<Renderer>();
-                rimMaterial = chestRenderer.sharedMaterials[1];
-                chestMaterial = chestRenderer.sharedMaterials[2];
+                Renderer chestRenderer = GameObject.Find("Dungeon_PuzzleChest").transform.Find("crystal").GetComponent<Renderer>();
+                edgeMaterial = chestRenderer.sharedMaterials[1];
+                faceMaterial = chestRenderer.sharedMaterials[0];
             }
             if (args.Length > 0)
             {
                 if (int.TryParse(args[0], out int id))
                 {
-                    rimMaterial.SetTexture("_MainTex", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{rimColors[id]}.png"));
-                    rimMaterial.SetTexture("_SpecularRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{rimShines[id]}.png"));
-                    chestMaterial.SetTexture("_MainTex", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{chestColors[id]}.png"));
+                    edgeMaterial.shader = Shader.Find("Unlit/Texture");
+                    edgeMaterial.SetTexture("_MainTex", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{crystalEdgeColors[id]}.png"));
+                    edgeMaterial.SetTexture("_RampTex", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{crystalEdgeRims[id]}.png"));
+                    edgeMaterial.SetTexture("_RimRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{crystalEdgeRimRamps[id]}.png"));
+
+                    // rimMaterial.SetTexture("_RimRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{rimColors[id]}.png"));
+                    faceMaterial.SetTexture("_SpecularRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{crystalFaceRims[id]}.png"));
+                    faceMaterial.SetTexture("_RimRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{crystalFaceRamps[id]}.png"));
+                    //rimMaterial.SetTexture("_SpecularRamp", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{rimShines[id]}.png"));
+                    //chestMaterial.SetTexture("_MainTex", ModCore.Utility.GetTextureFromFile($"{PluginInfo.PLUGIN_NAME}/Assets/{chestColors[id]}.png"));
                 }
             }
         }
