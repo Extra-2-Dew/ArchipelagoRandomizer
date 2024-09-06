25cba792a61f703278d3b5b25f2abab74acc7c61
GameWyrm <daronan97@hotmail.com>
2024-09-05 21:24:27 -0700

    Added support for all items

--- Code/ItemHandler.cs
+++ Code/ItemHandler.cs
@@ -9,8 +9,9 @@ namespace ArchipelagoRandomizer
 {
 	public class ItemHandler : MonoBehaviour
 	{
+		public static List<ItemData.Item> itemData;
+
 		private static ItemHandler instance;
-		private static List<ItemData.Item> itemData;
 		private static Dictionary<string, int> dungeonKeyCounts;
 		private static bool hasInitialized;
 
@@ -59,12 +60,18 @@ namespace ArchipelagoRandomizer
 		public int GetItemCount(ItemData.Item item, out bool isLevelItem)
 		{
 			isLevelItem = false;
-			Entity player = ModCore.Utility.GetPlayer();
+			if (item == null)
+			{
+				Plugin.Log.LogError("Illegal item count requested! Returning a count of 0.");
+				return 0;
+			}
+            Entity player = ModCore.Utility.GetPlayer();
 
-			if (player == null || mainSaver == null)
+            if (player == null || mainSaver == null)
 				return 0;
 
-			if (item.Type == ItemTypes.Key)
+
+            if (item.Type == ItemTypes.Key)
 			{
 				string dungeonName = item.ItemName.Substring(0, item.ItemName.IndexOf("Key") - 1).Replace(" ", "");
 
@@ -97,15 +104,18 @@ namespace ArchipelagoRandomizer
 				return keyCount;
 			}
 
-			List<string> levelItems = new() { "chain", "tome", "amulet", "headband", "tracker" };
-			List<string> countItems = new() { "shards", "raft", "potions", "evilKeys" };
+            if (item.ItemName.Contains("Outfit") && mainSaver.WorldStorage.HasData(item.SaveFlag))
+			{
+				return 1;
+            }
 
-			isLevelItem = item.Type == ItemTypes.Upgrade || levelItems.Contains(item.SaveFlag);
+            if (item.Type == ItemTypes.Card)
+			{
+				var cardSaver = mainSaver.GetSaver("/local/cards");
+				return cardSaver.LoadInt(item.SaveFlag);
+            }
 
-			if (isLevelItem || countItems.Contains(item.SaveFlag))
-				return player.GetStateVariable(item.SaveFlag);
-
-			return 0;
+            return player.GetStateVariable(item.SaveFlag);
 		}
 
 		public ItemData.Item GetItemData(string itemName)
@@ -124,7 +134,46 @@ namespace ArchipelagoRandomizer
 			return itemData.Find(item => item.Offset == offset);
 		}
 
-		public void GiveItem(ItemData.Item item)
+		public ItemData.Item GetItemData(APItem item)
+		{
+			return GetItemData(GetItemDataName(item));
+		}
+
+        public string GetItemDataName(APItem item)
+        {
+            string itemName = "";
+            string[] words = Regex.Split(item.ToString(), @"(?<!^)(?=[A-Z])");
+            if (words[0].Contains("Card"))
+            {
+                words[0] = words[0].Insert(4, " ");
+            }
+            else if (words[0] == "Connection") words[0] += " -";
+
+            switch (item)
+            {
+                case APItem.BoxOfCrayons:
+                case APItem.TombOfSimulacrumKey:
+                case APItem.TombOfSimulacrumKeyRing:
+                    words[1] = "of";
+                    break;
+                case APItem.BigOldPileOLoot:
+                    words[3] = "o'";
+                    break;
+                case APItem.Card30JennyBunUnemployed:
+                    words = ["Card 30 - Jenny Bun (Unemployed)"];
+                    break;
+                case APItem.Card33JennyBerryVacation:
+                    words = ["Card 33 - Jenny Berry (Vacation)"];
+                    break;
+            }
+
+            itemName = string.Join(" ", words);
+            Plugin.Log.LogMessage($"Returning \"{itemName}\"");
+
+            return itemName;
+        }
+
+        public void GiveItem(ItemData.Item item)
 		{
 			StartCoroutine(DoGiveItem(item));
 		}
@@ -506,4 +555,133 @@ namespace ArchipelagoRandomizer
 			}
 		}
 	}
+
+    public enum APItem
+    {
+        ProgressiveMelee,
+        ProgressiveForceWand,
+        ProgressiveDynamite,
+        ProgressiveIceRing,
+        ProgressiveChain,
+        ForceWandUpgrade,
+        DynamiteUpgrade,
+        IceRingUpgrade,
+        ChainUpgrade,
+        Roll,
+        ProgressiveTracker,
+        ProgressiveHeadband,
+        ProgressiveAmulet,
+        ProgressiveTome,
+        SecretShard,
+        ForbiddenKey,
+        Lockpick,
+        BoxOfCrayons,
+        CaveScroll,
+        PortalWorldScroll,
+        YellowHeart,
+        RaftPiece,
+        PillowFortKey,
+        PillowFortKeyRing,
+        SandCastleKey,
+        SandCastleKeyRing,
+        ArtExhibitKey,
+        ArtExhibitKeyRing,
+        TrashCaveKey,
+        TrashCaveKeyRing,
+        FloodedBasementKey,
+        FloodedBasementKeyRing,
+        PotassiumMineKey,
+        PotassiumMineKeyRing,
+        BoilingGraveKey,
+        BoilingGraveKeyRing,
+        GrandLibraryKey,
+        GrandLibraryKeyRing,
+        SunkenLabyrinthKey,
+        SunkenLabyrinthKeyRing,
+        MachineFortressKey,
+        MachineFortressKeyRing,
+        DarkHypostyleKey,
+        DarkHypostyleKeyRing,
+        TombOfSimulacrumKey,
+        TombOfSimulacrumKeyRing,
+        SyncopeKey,
+        SyncopeKeyRing,
+        BottomlessTowerKey,
+        BottomlessTowerKeyRing,
+        AntigramKey,
+        AntigramKeyRing,
+        QuietusKey,
+        QuietusKeyRing,
+        JennyDewOutfit,
+        SwimsuitOutfit,
+        TippsieOutfit,
+        LittleDudeOutfit,
+        TigerJennyOutfit,
+        IttleDew1Outfit,
+        DelinquintOutfit,
+        JennyBerryOutfit,
+        ApatheticFrogOutfit,
+        ThatGuyOutfit,
+        BigOldPileOLoot,
+        ImpossibleGatesPass,
+        Card1Fishbun,
+        Card2StupidBee,
+        Card3SafetyJenny,
+        Card4Shellbun,
+        Card5Spikebun,
+        Card6FeralGate,
+        Card7CandySnake,
+        Card8HermitLegs,
+        Card9Ogler,
+        Card10Hyperdusa,
+        Card11EvilEasel,
+        Card12Warnip,
+        Card13Octacle,
+        Card14Rotnip,
+        Card15BeeSwarm,
+        Card16Volcano,
+        Card17JennyShark,
+        Card18SwimmyRoger,
+        Card19Bunboy,
+        Card20Spectre,
+        Card21ReturnOfBrutus,
+        Card22Jelly,
+        Card23Skullnip,
+        Card24SlayerJenny,
+        Card25Titan,
+        Card26ChillyRoger,
+        Card27JennyFlower,
+        Card28Hexrot,
+        Card29JennyMole,
+        Card30JennyBunUnemployed,
+        Card31JennyCat,
+        Card32JennyMermaid,
+        Card33JennyBerryVacation,
+        Card34Mapman,
+        Card35Cyberjenny,
+        Card36LeBiadlo,
+        Card37Lenny,
+        Card38PasselCarver,
+        Card39Tippsie,
+        Card40IttleDew,
+        Card41NappingFly,
+        RandomBuff,
+        RandomDebuffTrap,
+        BeeTrap,
+        ConnectionFluffyFieldsToSweetwaterCoast,
+        ConnectionFluffyFieldsToFancyRuins,
+        ConnectionFluffyFieldsToStarWoods,
+        ConnectionFluffyFieldsToSlipperSlope,
+        ConnectionFluffyFieldsToPepperpainPrairie,
+        ConnectionSweetwaterCoastToFancyRuins,
+        ConnectionSweetwaterCoastToStarWoods,
+        ConnectionSweetwaterCoastToSlipperySlope,
+        ConnectionFancyRuinsToStarWoods,
+        ConnectionFancyRuinsToPepperpainPrairie,
+        ConnectionFancyRuinsToFrozenCourt,
+        ConnectionStarWoodsToFrozenCourt,
+        ConnectionSlipperySlopeToPepperpainPrairie,
+        ConnectionSlipperySlopeToLonelyRoad,
+        Potion
+    }
 }
